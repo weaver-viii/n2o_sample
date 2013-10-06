a6c674504111a389435a3fc30d0292fd756f870a
Maxim Sokhatsky <maxim@synrc.com>
2013-10-06 19:32:18 +0300

    fix delete

--- src/users.erl
+++ src/users.erl
@@ -12,5 +12,5 @@ populate(Users) -> ets:insert(users, Users).
 exists(Id) -> ets:member(users, wf:to_list(Id)).
 get() -> ets:tab2list(users).
 get(Id) -> [User] = ets:lookup(users, wf:to_list(Id)), User.
-delete(Id) -> ets:delete(users, Id).
+delete(Id) -> ets:delete(users, wf:to_list(Id)).
 post(Data) -> ets:insert(users, unmap(Data, #user{})).

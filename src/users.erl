80137b4d42f192cd56f3ae68315df61e41f761ad
Maxim Sokhatsky <maxim@synrc.com>
2013-10-06 12:37:57 +0300

    pure REST users NOW

--- src/users.erl
+++ src/users.erl
@@ -1,8 +1,12 @@
 -module(users).
+-include_lib("n2o/include/wf.hrl").
 -include("users.hrl").
 -compile(export_all).
 
-is_rest() -> true.
+?map(user).
+?unmap(user).
+?rest().
+
 init() -> ets:new(users, [public, named_table, {keypos, #user.id}]).
 populate(Users) -> ets:insert(users, Users).
 exists(Id) -> ets:member(users, wf:to_list(Id)).
@@ -11,18 +15,3 @@ get(Id) -> [User] = ets:lookup(users, wf:to_list(Id)), User.
 delete(Id) -> ets:delete(users, Id).
 post(User=#user{}) -> ets:insert(users, User);
 post(Data) -> ets:insert(users, unmap(Data, #user{})).
-
-map(#user{id = Id, name = Name, email = Email}) ->
-  [ {id,    wf:to_binary(Id)},
-    {name,  wf:to_binary(Name)},
-    {email, wf:to_binary(Email)} ].
-
-unmap([],                         O) -> O;
-unmap([{<<"id">>,    Id}    | T], O) -> unmap(T, O#user{id    = wf:to_list(Id)});
-unmap([{<<"name">>,  Name}  | T], O) -> unmap(T, O#user{name  = wf:to_list(Name)});
-unmap([{<<"email">>, Email} | T], O) -> unmap(T, O#user{email = wf:to_list(Email)});
-unmap([ _                   | T], O) -> unmap(T, O).
-
-%keys_allowed(Fields) ->
-%     lists:subtract(lists:map(fun wf:to_list/1, Fields),
-%                    lists:map(fun wf:to_list/1, record_info(fields, user))) == [].
